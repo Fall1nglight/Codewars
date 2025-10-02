aca55acf819ee5a39c7be3a7f9c001dd68a90423
Fall1ngLight <keruloszabolcs1@gmail.com>
2025-10-02 23:15:13 +0200

    ➕ add: ReplaceWithAlphabetPosition

---
+++ Codewars/6kyus/ReplaceWithAlphabetPosition.cs
@@ -0,0 +1,27 @@
+﻿namespace Codewars._6kyus;
+
+public class ReplaceWithAlphabetPosition
+{
+    public static string AlphabetPosition(string text)
+    {
+        // 1. iterate over the given string
+        // 2. check if the current char is letter
+        // 3. if it is, get its position in the alphabet and return it
+        // 4. if it is NOT, return an empty string
+        // 5. filter out empty strings
+        // 6. join elements by a space
+
+        var result = string.Join(
+            " ",
+            text.Select(x =>
+                    char.IsLetter(x) ? (char.ToLower(x) - 'a' + 1).ToString() : string.Empty
+                )
+                .Where(x => !string.IsNullOrEmpty(x))
+        );
+
+        return result;
+
+        // better solution, because we convert the text to lowercase in the beginning and filter before iteration
+        // return string.Join(" ", text.ToLower().Where(char.IsLetter).Select(x => x - 'a'+1));
+    }
+}
---
+++ Tests/6kyus/ReplaceWithAlphabetPositionTest.cs
@@ -0,0 +1,20 @@
+﻿using static Codewars._6kyus.ReplaceWithAlphabetPosition;
+
+namespace Tests._6kyus;
+
+[TestFixture]
+public class ReplaceWithAlphabetPositionTest
+{
+    [Test, Order(1)]
+    public void SampleTest()
+    {
+        Assert.That(
+            AlphabetPosition("The sunset sets at twelve o' clock."),
+            Is.EqualTo("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
+        );
+        Assert.That(
+            AlphabetPosition("The narwhal bacons at midnight."),
+            Is.EqualTo("20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20")
+        );
+    }
+}
