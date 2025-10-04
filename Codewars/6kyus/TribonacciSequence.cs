1ec23c77f6d01b8bf934bf530a9a99b6b7a23dda
Fall1ngLight <keruloszabolcs1@gmail.com>
2025-10-04 19:03:41 +0200

    ➕ add: TribonacciSequence

---
+++ Codewars/6kyus/TribonacciSequence.cs
@@ -0,0 +1,34 @@
+﻿namespace Codewars._6kyus;
+
+public class TribonacciSequence
+{
+    public double[] Tribonacci(double[] signature, int n)
+    {
+        if (n == 0)
+            return [];
+
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i < 3)
+                result[i] = signature[i];
+            else
+                result[i] = result[i - 3] + result[i - 2] + result[i - 1];
+        }
+
+        return result;
+
+        // double[] result = new double[n < 4 ? 3 + n : n];
+        //
+        // result[0] = signature[0];
+        // result[1] = signature[1];
+        // result[2] = signature[2];
+        //
+        // for (int i = 3; i < n; i++)
+        //     result[i] = result[i - 1] + result[i - 2] + result[i - 3];
+        //
+        // Array.Resize(ref result, n);
+        // return result;
+    }
+}
---
+++ Tests/6kyus/TribonacciSequenceTest.cs
@@ -0,0 +1,43 @@
+﻿using Codewars._6kyus;
+
+namespace Tests._6kyus;
+
+[TestFixture]
+public class TribonacciSequenceTest
+{
+    private TribonacciSequence _variabonacci;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _variabonacci = new TribonacciSequence();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _variabonacci = null!;
+    }
+
+    [Test]
+    public void Tests()
+    {
+        Assert.That(
+            _variabonacci.Tribonacci(new double[] { 1, 1, 1 }, 10),
+            Is.EqualTo(new double[] { 1, 1, 1, 3, 5, 9, 17, 31, 57, 105 })
+        );
+        Assert.That(
+            _variabonacci.Tribonacci(new double[] { 0, 0, 1 }, 10),
+            Is.EqualTo(new double[] { 0, 0, 1, 1, 2, 4, 7, 13, 24, 44 })
+        );
+        Assert.That(
+            _variabonacci.Tribonacci(new double[] { 0, 1, 1 }, 10),
+            Is.EqualTo(new double[] { 0, 1, 1, 2, 4, 7, 13, 24, 44, 81 })
+        );
+
+        Assert.That(
+            _variabonacci.Tribonacci(new double[] { 1, 1, 1 }, 1),
+            Is.EqualTo(new double[] { 1 })
+        );
+    }
+}
