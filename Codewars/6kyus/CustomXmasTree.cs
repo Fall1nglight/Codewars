4720764372db3aa26345d6aeb30184e434fcbf5b
Fall1ngLight <keruloszabolcs1@gmail.com>
2025-10-04 14:18:22 +0200

    ➕ add: CustomXmasTree

---
+++ Codewars/6kyus/CustomXmasTree.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Codewars._6kyus;
+
+public class CustomXmasTree
+{
+    public static string CustomChristmasTree(string chars, int n)
+    {
+        int customIdx = 0;
+        StringBuilder sb = new StringBuilder();
+
+        // iterate n times to construct the leaves
+        for (int lineNum = 1; lineNum <= n; lineNum++)
+        {
+            // calculate the number of spaces needed to center the current line
+            // the widest line has (n characters + n-1 spaces) = 2*n - 1 total width
+            // the current line has (lineNum characters + lineNum-1 spaces) = 2*lineNum - 1 total width
+            // to center the line, we need (2*n - 1 - (2*lineNum - 1)) / 2 = n - lineNum spaces
+            sb.Append(new string(' ', n - lineNum));
+
+            // construct the current line, which will consist of 'lineNum' chars + separators
+            for (int charCount = 0; charCount < lineNum; charCount++)
+                sb.Append(
+                    chars[customIdx++ % chars.Length] + (charCount < lineNum - 1 ? " " : "\n")
+                );
+        }
+
+        int trunkSize = n / 3;
+
+        // construct the trunks
+        // each trunk will need n-1 spaces to be centered
+        for (int i = 0; i < trunkSize; i++)
+            sb.Append(new string(' ', n - 1) + (i < trunkSize - 1 ? "|\n" : "|"));
+
+        return sb.ToString();
+    }
+}
---
+++ Tests/6kyus/CustomXmasTreeTest.cs
@@ -0,0 +1,49 @@
+﻿using static Codewars._6kyus.CustomXmasTree;
+
+namespace Tests._6kyus;
+
+[TestFixture]
+public class CustomXmasTreeTest
+{
+    [Test]
+    public void BasicTest()
+    {
+        Assert.That(
+            CustomChristmasTree("*@o", 3),
+            Is.EqualTo("  *\n" + " @ o\n" + "* @ o\n" + "  |")
+        );
+
+        Assert.That(
+            CustomChristmasTree("*@o", 6),
+            Is.EqualTo(
+                "     *\n"
+                    + "    @ o\n"
+                    + "   * @ o\n"
+                    + "  * @ o *\n"
+                    + " @ o * @ o\n"
+                    + "* @ o * @ o\n"
+                    + "     |\n"
+                    + "     |"
+            )
+        );
+
+        Assert.That(
+            CustomChristmasTree("1234", 6),
+            Is.EqualTo(
+                "     1\n"
+                    + "    2 3\n"
+                    + "   4 1 2\n"
+                    + "  3 4 1 2\n"
+                    + " 3 4 1 2 3\n"
+                    + "4 1 2 3 4 1\n"
+                    + "     |\n"
+                    + "     |"
+            )
+        );
+
+        Assert.That(
+            CustomChristmasTree("123456789", 3),
+            Is.EqualTo("  1\n" + " 2 3\n" + "4 5 6\n" + "  |")
+        );
+    }
+}
